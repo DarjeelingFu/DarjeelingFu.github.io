89b862f323807780268b02b26ebcf346f51f3a76
FuXiangyu <2437402695@qq.com>
2020-10-22 18:23:05 +0800

    适配移动端

--- static/js/image_cut.js
+++ static/js/image_cut.js
@@ -1,8 +1,14 @@
 function cutImage(id, width, height){
-	var img = document.getElementById(id);
+	var img = document.getElementById( id);
 	var ig = $(img).clone();
 	var imgWidth = $(ig)[0].width
 	var imgHeight = $(ig)[0].height
+	
+	console.log(img)
+	console.log(imgWidth)
+	console.log(imgHeight)
+	console.log(width)
+	console.log(height)
 	
 	if(width >= height){
 		if(imgWidth >= imgHeight){
